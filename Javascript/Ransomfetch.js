b600a6a51347f8702afb983b11249731dad4720e
Dumi Mofolo <Dumisanimofolo@gmail.com>
2024-10-03 02:20:45 +0200

    Design Component Part 2

--- Javascript/Ransomfetch.js
+++ Javascript/Ransomfetch.js
@@ -1,6 +1,7 @@
 const ransomURL = 'https://api.ransomware.live/allcyberattacks';
 // this API contains datat about different cybersecurity attacks done on different victims of the world by IP address and different ransomware attacks
 
+
 fetch(ransomURL)
   .then(response => response.json())
   .then(data => {
