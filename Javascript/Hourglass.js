6a0d600d76626376c32d724e0304a0eadc70e84c
Dumi Mofolo <Dumisanimofolo@gmail.com>
2024-10-05 22:23:17 +0200

    Loadinggg Screen Update
    -Added loading screen update

---
+++ Javascript/Hourglass.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    document.getElementById("loading-screen").style.display = "none"; // hiding the hourglass icon after the dom has fully loaded
+
+    console.log("yes i am loadingg");
+})
