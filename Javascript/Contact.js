6bb32c468415ad04e9d7d61d6aeb042110f61fe5
Dumi Mofolo <Dumisanimofolo@gmail.com>
2024-10-31 11:14:51 +0200

    Form Validation and Submission Update

--- Javascript/Contact.js
+++ Javascript/Contact.js
@@ -1,16 +1,36 @@
 const contactForm = document.querySelector(".Contact");
 const nameInput = document.getElementById("name");
 const emailInput = document.getElementById("email");
+const phoneNumberInput = document.getElementById("phone-number");
+const phoneWarning = document.getElementById("phone-warning");
 const messageInput = document.getElementById("message");
 const submitBtn = document.getElementById("submitBtn");
 const feedbackMsg = document.getElementById("feedback");
 
 
 function validateForm() {
+    // required fields validation:
     if (nameInput.value && emailInput.value && messageInput.value) {
         submitBtn.disabled = false; // btn will  work if the required fileds are fully filled in
     } else {
         submitBtn.disabled = true; // btn will not  work  if the required fields are not filled in 
+    }
+
+    //phone number validation 
+    if(phoneNumberInput.validity.patternMismatch || phoneNumberInput.value.length < 10){
+        phoneWarning.style.display = "inline";
+        phoneWarning.innerText = "Please enter a valid 10-digit phone number"
+    }
+    else{
+        phoneWarning.style.display = "none";
+    }
+
+    //email validity 
+    if(!emailInput.validity.valid){
+        emailInput.setCustomValidity("Please enter an email with the correct extension: either '@gmail.com or @icloud.com'");
+    }
+    else{
+        emailInput.setCustomValidity = "";
     }
 }
 
@@ -18,6 +38,7 @@ function validateForm() {
 nameInput.addEventListener("input", validateForm);
 emailInput.addEventListener("input", validateForm);
 messageInput.addEventListener("input", validateForm);
+phoneNumberInput.addEventListener("input", validateForm);
 
 // Form submission handling
 contactForm.addEventListener("submit", (event) => {
@@ -29,17 +50,23 @@ contactForm.addEventListener("submit", (event) => {
         return; // Stop the function if fields are not completed
     }
 
+
+    
+
     // Save data to local storage
     localStorage.setItem("name", nameInput.value);
     localStorage.setItem("email", emailInput.value);
+    localStorage.setItem("phone-number", phoneNumberInput.value);
     localStorage.setItem("message", messageInput.value);
 
     // Clear form fields
     nameInput.value = "";
     emailInput.value = "";
     messageInput.value = "";
+    phoneNumberInput.value = "";
 
     // Reset button state and show feedback message
     submitBtn.disabled = true;
-    feedbackMsg.innerText = "Lovely! Your message has been submitted successfully :) Dumi Mofolo has been alerted and will get back to you shortly!";
+    feedbackMsg.innerText = "Lovely! Your message has been submitted successfully :)";
+    phoneWarning.style.display = "none";
 });
