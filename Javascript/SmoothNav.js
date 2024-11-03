aeaf11668704c86b14316c0a2814346977c3a836
Dumi Mofolo <Dumisanimofolo@gmail.com>
2024-11-03 02:24:40 +0200

    Smooth Nav Bar section Update
    -Added a side bar scrolling to my essay section so its easier to scroll to each subheading as opposed having to read the whole essay.

---
+++ Javascript/SmoothNav.js
@@ -0,0 +1,12 @@
+
+//here im basically finding each anchor tag wjich has the subheading that would be clikable and take the users to that section of the essay
+document.querySelectorAll(".Sidebar a").forEach(anchor =>{
+    anchor.addEventListener("click", (e)=> {
+    e.preventDefault();// prevent page from reloading
+    const target = document.querySelector(this.getAttribute('href'));
+    window.scrollTo({
+        top: target.offsetTop - 20,
+        behavior: "smooth"
+    });
+    });
+});
